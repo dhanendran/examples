24925be97e99c204f1bd92439812056e000a423c
Dhanendran <dhanendran.rajagopal@gmail.com>
2019-03-25 16:07:47 +0000

    Create bc.js

---
+++ bc.js
@@ -0,0 +1,23 @@
+videojs.registerPlugin('ampCustomParam', function() {
+
+  var myPlayer = this;
+
+  function getQuerystring(key, default_) {
+    var regex, qs;
+    if (default_ == null) default_ = "";
+    key = key.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    regex = new RegExp("[\\?&]" + key + "=([^&#]*)");
+    qs = regex.exec(window.location.href);
+    if (qs === null) {
+        return default_;
+    } else {
+        return qs[1];
+    }
+}
+
+
+  // Get the query string and un-URL encode it
+  myPlayer.ima3.settings.serverUrl = myPlayer.ima3.settings.serverUrl +
+  "&value1=" + getQuerystring("val1");
+
+});
